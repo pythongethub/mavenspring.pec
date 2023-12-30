db6a9b94bb456fbf1cd9c672c687453c2a4bbdff
Murali8106 <kakanurumuralidharreddy@gmail.com>
2023-12-30 18:28:07 -0500

    firstphase

---
+++ src/main/java/com/ir7/devops/helloworld.java
@@ -0,0 +1,6 @@
+package src.main.java.com.ir7.devops;
+class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
---
+++ src/main/java/com/ir7/devops/utils.java
@@ -0,0 +1,7 @@
+package src.main.java.com.ir7.devops;
+
+public class utils{
+    public String getName(){
+        return "welcome to ir7";
+    }
+}
